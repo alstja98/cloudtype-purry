6c28c34c9d415732b3182ad7de525c6d466bc7ea
tnsqjahong <cruseight47@naver.com>
2023-01-18 21:33:00 +0900

    fix: add prompt, position model

--- core/models.py
+++ core/models.py
@@ -6,8 +6,10 @@ class Images(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey('User', models.DO_NOTHING, db_column='user_id')
     path = models.CharField(max_length=1000)
+    prompt_id = models.ForeignKey('Prompt', models.DO_NOTHING, db_column='prompt_id')
+    position = models.CharField(max_length=1000)
     update_time = models.DateTimeField(auto_now=True)
-
+    
 
     class Meta:
         managed = False
@@ -24,3 +26,14 @@ class User(models.Model):
     class Meta:
         managed = False
         db_table = 'user'
+        
+class Prompt(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    prompt = models.CharField(max_length=500)
+    update_time = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        managed = False
+        db_table = 'prompt'
